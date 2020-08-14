a89a4298948373eaed221d8255a03ca25b0cbc15
Kristen Howton <kristen.howton@gmail.com>
2020-08-14 16:42:19 -0500

    able to compute grades

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,20 @@
+﻿//need namespace to not conflict with MS
+namespace Gradebook
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //used to instantiate a class, contructor method
+            Book book = new Book("Gradebook");
+            book.AddGrade(90.1);
+            book.AddGrade(80.4);
+            book.AddGrade(70.2);
+            book.ShowStatistics();
+
+        }
+
+    }
+}
